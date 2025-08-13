71ad3966d3e8c3ea5107ed8e7d689bb1374a7c14
Ashutosh! <ashutoshpatraybl@gmail.com>
2025-08-13 22:59:18 +0530

    Add files via upload

--- leetcode.py
+++ leetcode.py
@@ -1,15 +1,11 @@
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort()  
-        merged = []
-        prev = intervals[0]
-
-        for i in range(1, len(intervals)):
-            if intervals[i][0] <= prev[1]:  
-                prev[1] = max(prev[1], intervals[i][1])  
-            else:
-                merged.append(prev)
-                prev = intervals[i]
-
-        merged.append(prev)
-        return merged    
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        prefix = strs[0]
+        for string in strs[1:]:
+            while string.find(prefix) != 0:
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix  
